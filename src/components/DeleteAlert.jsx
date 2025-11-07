a4cd86db9c448543961dcbf1181b848ed16597ed
Alvarizqi <alvarizki80@students.unnes.ac.id>
2025-11-08 00:56:38 +0700

    fix: change text color in DeleteAlert component for improved readability

--- src/components/DeleteAlert.jsx
+++ src/components/DeleteAlert.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const DeleteAlert = ({ content, onDelete }) => {
   return (
     <div className="flex flex-row items-center justify-center min-h-[120px] w-full gap-4 px-6">
-      <p className="text-md font-semibold text-white text-left flex-1 break-words">
+      <p className="text-md font-semibold text-gray-800 text-left flex-1 break-words">
         {content}
       </p>
       <button className="add-btn add-btn-fill" onClick={onDelete}>
